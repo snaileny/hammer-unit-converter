98f680b927f4ceee72e32258c99c11b7e3b53f7c
snaileny <emirerek@hotmail.com.tr>
2022-05-04 20:47:57 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-import UnitSelector from "/hammer-unit-converter/js/modules/unitSelector.js"; 
-import InputHandler from "/hammer-unit-converter/js/modules/inputHandler.js";
-import HistoryManager from "/hammer-unit-converter/js/modules/historyManager.js";
-import Converter from "/hammer-unit-converter/js/converter.js";
-import {applyOn, changeTextContent, changePlaceholder, dispatchKeyboardEvent} from "/hammer-unit-converter/js/helpers.js";
+import UnitSelector from "../js/modules/unitSelector.js"; 
+import InputHandler from "../js/modules/inputHandler.js";
+import HistoryManager from "../js/modules/historyManager.js";
+import Converter from "../js/converter.js";
+import {applyOn, changeTextContent, changePlaceholder, dispatchKeyboardEvent} from "../js/helpers";
 
 document.addEventListener('DOMContentLoaded', () => {
 
